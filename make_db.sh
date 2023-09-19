e15f942c4154ab6fc5caf1ac60361625b5eb3c9f
Adam Kassimi <23499836+adamkasmi@users.noreply.github.com>
2023-09-19 19:16:56 +0200

    Update make_db.sh

--- make_db.sh
+++ make_db.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <username> <password> <dbname>"
+    exit 1
+fi
+
+USERNAME=$1
+PASSWORD=$2
+DBNAME=$3
 LOGFILE="$(pwd)/postgres_setup.log"
+
 echo "Logging details to $LOGFILE..."
 
 # Check if postgresql is installed
@@ -19,35 +28,35 @@ else
     echo "PostgreSQL is already installed." | tee -a $LOGFILE
 fi
 
-# Check if database "carddb" exists
-DB_EXISTS=$(sudo -u postgres psql -lqt | cut -d \| -f 1 | grep -w carddb | wc -l)
+# Check if database exists
+DB_EXISTS=$(sudo -u postgres psql -lqt | cut -d \| -f 1 | grep -w $DBNAME | wc -l)
 
 if [ "$DB_EXISTS" -eq "0" ]; then
-    echo "Database carddb does not exist. Creating..." | tee -a $LOGFILE
+    echo "Database $DBNAME does not exist. Creating..." | tee -a $LOGFILE
     
     # Create the database
-    sudo -u postgres createdb carddb
+    sudo -u postgres createdb $DBNAME
 else
-    echo "Database carddb already exists." | tee -a $LOGFILE
+    echo "Database $DBNAME already exists." | tee -a $LOGFILE
 fi
 
-# Check if user "carduser" exists
-USER_EXISTS=$(sudo -u postgres psql -tAc "SELECT 1 FROM pg_roles WHERE rolname='carduser'")
+# Check if user exists
+USER_EXISTS=$(sudo -u postgres psql -tAc "SELECT 1 FROM pg_roles WHERE rolname='$USERNAME'")
 
 if [ -z "$USER_EXISTS" ]; then
-    echo "User carduser does not exist. Creating..." | tee -a $LOGFILE
+    echo "User $USERNAME does not exist. Creating..." | tee -a $LOGFILE
     
     # Create user and set password
-    sudo -u postgres psql -c "CREATE USER carduser WITH PASSWORD 'password123';"
-    sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE carddb TO carduser;"
+    sudo -u postgres psql -c "CREATE USER $USERNAME WITH PASSWORD '$PASSWORD';"
+    sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $DBNAME TO $USERNAME;"
 else
-    echo "User carduser already exists." | tee -a $LOGFILE
+    echo "User $USERNAME already exists." | tee -a $LOGFILE
 fi
 
 echo "Setup details:" | tee -a $LOGFILE
-echo "Database Name: carddb" | tee -a $LOGFILE
-echo "Username: carduser" | tee -a $LOGFILE
-echo "Password: password123" | tee -a $LOGFILE
-echo "Connection String for Flask: postgresql://carduser:password123@localhost/carddb" | tee -a $LOGFILE
+echo "Database Name: $DBNAME" | tee -a $LOGFILE
+echo "Username: $USERNAME" | tee -a $LOGFILE
+echo "Password: $PASSWORD" | tee -a $LOGFILE
+echo "Connection String for Flask: postgresql://$USERNAME:$PASSWORD@localhost/$DBNAME" | tee -a $LOGFILE
 
 echo "Done." | tee -a $LOGFILE
